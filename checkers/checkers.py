ac8a0a368f80c0489bb422755bc040799d96e5fe
jakobiannn <mozilla322@gmail.com>
2020-10-21 21:40:08 +0300

    add must moves without tests

---
+++ checkers/checkers.py
@@ -0,0 +1,156 @@
+from base import state
+
+class state_giveaway(state):
+	#'''Значение бесконечности для оценочной функции'''
+	infinity = 100
+
+
+    #lastmove = (x1, y1, x2, y2, player)
+	#'''список игроков - крестик и нолик'''
+	players = ["W", "B"]
+	cheskers = {'W': ['W', 'WQ'], 'B': ['B', 'BQ'],}
+
+	#'''противники'''
+	opponent = {"W":"B", "B":"W"}
+	def __init__(self, value=None):
+		#'''Конструктор класса, инициализация полей'''
+		# если value==None, то генерируем
+		# пустое поле
+		if value:
+			self.value = value
+		else:
+            # иначе создаем готовое поле
+			self.value = field
+		print('Game is started!')
+
+	def do_move(self, move, eaten_figures = None):
+	# ход представляет собой кортеж
+	# (x1, y1, x2, y2, player)
+	# где
+	# - x1, y1 - начальные координаты шашки
+	# - x2, y2 - конечные координаты
+	# - player - игрок, который делает ход
+	# координаты отсчитываются от 0 до 7
+	# eaten_figures - список из списков с координатами съеденных фигур
+		x1, y1, x2, y2, player = move
+		self.value[x1][y1] = 0
+		self.value[x2][y2] = player
+		if eaten_figures != None:
+			for figure in eaten_figures:
+				x, y = figure
+				self.value[x][y] = 0
+
+
+	def undo_move(self, move, eaten_figures):
+		# ход представляет собой кортеж
+		# (row, col, player)
+		# где
+		# - row, col - координаты клетки,
+		#              в которую ставится символ
+		# - player - игрок, который делает ход
+		x1, y1, x2, y2, player = move
+		# при отмене хода мы ставим в ячейку
+		# пустое значение
+		self.value[x1][y1] = player
+		self.value[x2][y2] = 0
+		if eaten_figures != None:
+			for figure in eaten_figures:
+				x, y = figure
+				self.value[x][y] = opponent[player]
+
+	def is_win(self, player):
+		#'''Проверка, что игрок player выиграл'''
+		enemyFigures = 0
+		for lines in self.value:
+			for row in lines:
+				if row == self.opponent[player]:
+					enemyFigures += 1
+		if enemyFigures == 0:
+			return True
+		return False
+	#def kingMoves() - для оценки возможных ходов дамки
+
+	#вспомогательная функция для get_moves,
+	#проверим есть ли обязательные ходы и если их нет,
+	#то занесем все обычные
+	def checkSlot(self, lines, row, player, moves, eaten_figures, a, b):
+		#проверим, существуют ли ячейки поля по диагонали
+		if(lines +  a >= 0 and row + b <= 7):
+			if (self.value[lines + a][row + b] == self.opponent[player]):
+				#проверим, можно ли съесть шашку(существует ли дальше поле)
+				#с последующей проверкой с помощью рекурсии
+				#ячейка должна как существовать, так и не являться съеденной
+				if([lines + a, row + b] not in eaten_figures):
+					if(lines + 2*a >= 0 and row + 2*b <= 7):
+						moves += [lines, row]
+						eaten_figures += [[lines + a, row + b]]
+						#записываем отдельно координаты последнего нахождения шашки
+						last = [lines + 2*a, row + 2*b]
+						#проверяем, есть ли ещё доступные ходы
+						potentialMoves, _ = self.mustHaveMoves(lines + 2*a, row + 2*b, player, moves, eaten_figures)
+						if(potentialMoves != []):
+							self.mustHaveMoves(lines + 2*a, row + 2*b, player, moves, eaten_figures)
+						else:
+							#делаем для того, чтобы добавлять список в конец хода
+							moves += last
+							moves += player
+							moves += [moves[-5:]]
+							del moves[-6:-1]
+		return moves, eaten_figures
+
+	def mustHaveMoves(self, lines, row, player, moves = [], eaten_figures = []):
+		moves, eaten_figures += self.checkSlot(lines, row, player, moves, eaten_figures, 1, 1)
+		moves, eaten_figures += self.checkSlot(lines, row, player, moves, eaten_figures, 1, -1)
+		moves, eaten_figures += self.checkSlot(lines, row, player, moves, eaten_figures, -1, 1)
+		moves, eaten_figures += self.checkSlot(lines, row, player, moves, eaten_figures, -1, -1)
+		return moves, eaten_figures
+
+	def commonMoves(self, lines, row, player):
+		moves = []
+		#условие, что шашка не стоит в верхней строчке и левом/правом столбце
+		#аналогично для черных в нижней строчке и нижнем левом/правом
+		if(player == 'W'):
+			if(lines - 1 >= 0 and row + 1 <= 7):
+				if (self.value[lines - 1][row + 1] != player):
+						moves += [[lines, row, lines - 1, row + 1, player]]
+			#или левом столбце
+			if(lines - 1 <= 7 and row - 1 >= 0):
+				if (self.value[lines - 1][row - 1] != player):
+						moves += [[lines, row, lines - 1, row - 1, player]]
+		elif(player == 'B'):
+			if(lines + 1 >= 0 and row + 1 <= 7):
+				if (self.value[lines - 1][row + 1] != player):
+						moves += [[lines, row, lines + 1, row + 1, player]]
+			#или левом столбце
+			if(lines + 1 <= 7 and row - 1 >= 0):
+				if (self.value[lines - 1][row - 1] != player):
+						moves += [[lines, row, lines + 1, row - 1, player]]
+		return moves
+
+	def get_moves(self, player):
+		# если ситуация выигрышная или проигрышная
+		# то ходов нет
+		if self.is_win(player) or self.is_win(self.opponent[player]):
+			return []
+		#получим список обязательных ходов для каждой шашки
+		#если есть обязательные ходы, то вернем их
+		moves = []
+		for lines in range(8):
+			for row in range(8):
+				if self.value[lines][row] in cheskers[player]:
+					moves, eaten_figures += self.mustHaveMoves(lines, row, player)
+		#если обязательных нет
+		#получим список обычных ходов
+		if(moves == []):
+			for lines in range(8):
+				for row in range(8):
+					if self.value[lines][row] in cheskers[player]:
+						moves += self.commonWhiteMoves(lines, row, player)
+		return moves
+
+
+
+
+	def score(self, player):
+		#'''Расчет оценочной функции'''
+		raise NotImplementedError
---
+++ checkers/test.py
@@ -0,0 +1,23 @@
+from giveaway import state_giveaway
+# # from minimax import bestmove
+# from negmax import bestmove
+
+# if __name__ == "__main__":
+#     s = state_xo()
+#     level = 6
+#     player, opponent = "X", state_xo.opponent["X"]
+
+#     move = bestmove(s, level, player, opponent)
+
+#     print(f"Best move is: ", move)
+global field
+field = [[0,"B",0,"B",0,"B",0,"B"],
+        ["B",0,"B",0,"B",0,"B",0],
+        [0,"B",0,"B",0,"B",0,"B"],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, "B", 0, 0, 0, 0, 0, 0],
+        ["W",0,"W",0,"W",0,"W",0],
+        [0,"W",0,"W",0,"W",0,"W"],
+        ["W",0,"W",0,"W",0,"W",0]]
+game = state_giveaway(field)
+print(game.get_moves("W"))
